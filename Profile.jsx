d2ca0b62bfa7111a2d4ef4aa19be5654ea1249de
Saood Faisal <87062090+faisalkhan180@users.noreply.github.com>
2024-06-26 07:58:27 -0700

    Update Profile.jsx

--- Profile.jsx
+++ Profile.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function Profile({getAdjective}) {
+export default function Profile({getAdjective, adjective}) {
+  console.log(getAdjective)
   return (
     <div>
-      <h1>he is very {getAdjective}</h1>
+      <h1>he is very {adjective}</h1>
     </div>
   )
 }
